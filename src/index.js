e3b75f88d17ec34fb8a3d90fe2ad934bfa40182d
iSagdiev <jay.iskander@gmail.com>
2020-10-11 19:28:41 +0500

    fix: task uploading.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let a = '';
+    n = Math.abs(n);
+    a = n.toString().split('').reverse().join('');
+    a = Number.parseInt(a);
+    return(a) ;
 }
+
+
